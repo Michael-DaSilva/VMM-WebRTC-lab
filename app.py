f89bd59b934f7d9889419ac2cb2bac9188da3527
Michael-DaSilva <michael.dasilva@heig-vd.ch>
2021-05-24 17:05:09 +0200

    function handle_p2pmessage done

--- app.py
+++ app.py
@@ -53,17 +53,25 @@ def handle_join(room_name):
         emit("full", room_name)
 
 def handle_p2pmessage(msg_type, content):
-    # *** TODO ***: Get the user_id from the request variable (see handle_join)
-    # *** TODO ***: Get the room_name of the user from rooms_db
-    print(f"Received {msg_type} message: {content} from user: {user_id} in room {room_name}")
+    user_id = request.sid
+    room_name = rooms_db[user_id]
 
-    # *** TODO ***: Broadcast the message to existing client in the SocketIO room.
-    #               Exclude the sender of the orignal message.
+    print(f"Received {msg_type} message: {content} from user: {user_id} in room {room_name}")
+    emit(msg_type, content, broadcast=True, include_self=False)
 
 
 # *** TODO ***: Create a message handler for 'invite' messages 
+@socketio.on('invite')
+def handle_invite(room_name):
+
 # *** TODO ***: Create a message handler for 'ok' messages 
+@socketio.on('ok')
+def handle_ok(room_name):
+
 # *** TODO ***: Create a message handler for 'ice_candidate' messages 
+@socketio.on('ice_candidate')
+def handle_bye(room_name):
+    
 
 @socketio.on('bye')
 def handle_bye(room_name):
